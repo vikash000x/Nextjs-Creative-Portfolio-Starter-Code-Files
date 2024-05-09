3f64a91249e673f93f7fcdcf9bbd6a82ebadf430
vikash sinha <103680992+vikash000x@users.noreply.github.com>
2024-05-09 11:24:12 +0530

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -8,7 +8,7 @@ const AboutDetails = () => {
       <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
         <ItemLayout
           className={
-            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+            " col-span-full lg:col-span-12 row-span-2 flex-col items-start"
           }
         >
           <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
@@ -23,7 +23,7 @@ const AboutDetails = () => {
           </p>
         </ItemLayout>
 
-        <ItemLayout
+{/*         <ItemLayout
           className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
         >
           <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
@@ -38,7 +38,7 @@ const AboutDetails = () => {
             4+{" "}
             <sub className="font-semibold text-base">years of experience</sub>
           </p>
-        </ItemLayout>
+        </ItemLayout> */}
 
         <ItemLayout
           className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
